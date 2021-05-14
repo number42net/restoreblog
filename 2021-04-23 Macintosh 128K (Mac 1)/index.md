6d98c252f513f6bf6808507ce55e62a9e474195b
number42net <github@number42.net>
2021-05-14 14:48:37 +0200

    Updates

---
+++ 2021-04-23 Macintosh 128K (Mac 1)/index.md
@@ -0,0 +1,15 @@
+# Macintosh 128K (Mac 1)
+
+Intro
+
+##### Work done:
+
+* Line item 1
+* Line Item 2
+
+##### Planned work:
+
+* Line item 1
+* Line Item 2
+
+# Exterior inspection:
--- scripts/new_project.sh
+++ scripts/new_project.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 mkdir -p "$@"/fullsize
-touch "$@"/project.yaml
-touch "$@"/index.md
+
+date=$(echo $@ | awk '{ print $1 }')
+name=$(echo $@ | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')
+
+cat > "$@"/project.yaml <<- EOM
+project:
+  published: false
+  name: "$name"
+  status: In progress
+  start_date: "$date"
+  thumbnail: img_001.jpg
+  description: 
+    - "Short description"
+    - ""
+    - "- List item 1"
+EOM
+
+cat > "$@"/index.md <<- EOM
+# $name
+
+Intro
+
+##### Work done:
+
+* Line item 1
+* Line Item 2
+
+##### Planned work:
+
+* Line item 1
+* Line Item 2
+
+# Exterior inspection:
+EOM
